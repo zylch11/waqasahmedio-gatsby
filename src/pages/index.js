8a36070811e64ea37f6d71fa5d0b0100f1c804de
zylch11 <waqas.ahmed11@protonmail.com>
2022-03-03 01:27:46 +0500

    Using a plugin to add an image to the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Indexpage = () => {
   return (
     <main>
-      <Layout pageTitle='Home Page'>
+      <Layout pageTitle="Home Page">
         <p>I'm making this by following the Official Gatsby Tutorial.</p>
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="../images/image.jpg"
+        />
       </Layout>
     </main>
-  )
-}
+  );
+};
 
-export default Indexpage
+export default Indexpage;
